8af472d32360783aac2216422156b48bd7ad0418
vedivero <vedivero@github.com>
2024-09-15 16:57:26 +0900

    몽고DB 연결 테스트

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+const mongoose = require('mongoose');
+mongoose
+   .connect('mongodb+srv://vedivero:1234@cluster0.hmtbdny.mongodb.net/', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+   })
+   .then(() => console.log('MongoDB Connected'))
+   .catch((err) => console.log(err));
+
 app.get('/', (req, res) => res.send('Hello World'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
