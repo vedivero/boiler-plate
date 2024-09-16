c4ae3a1d6c78b7a8934f5f1239b8ec3b2646a19c
vedivero <vedivero@github.com>
2024-09-16 22:02:59 +0900

    토큰 생성, jsonwebtoken

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const bodyparser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const config = require('./config/key');
 const { User } = require('./models/User');
 const mongoose = require('mongoose');
 
-//application/x-www-form-urlencoded
+// application/x-www-form-urlencoded
 app.use(bodyparser.urlencoded({ extended: true }));
-//application json
+// application/json
 app.use(bodyparser.json());
+app.use(cookieParser());
 
 mongoose
    .connect(config.mongoURI, {
@@ -21,6 +23,7 @@ mongoose
 
 app.get('/', (req, res) => res.send('Hello World'));
 
+// 회원 가입
 app.post('/register', async (req, res) => {
    const user = new User(req.body);
    try {
@@ -28,6 +31,56 @@ app.post('/register', async (req, res) => {
       return res.status(200).json({ success: true });
    } catch (error) {
       return res.json({ success: false, error });
+   }
+});
+
+// 로그인
+app.post('/login', async (req, res) => {
+   try {
+      // 요청된 이메일을 데이터베이스에서 찾기
+      const user = await User.findOne({ email: req.body.email });
+      if (!user) {
+         return res.json({
+            loginSuccess: false,
+            message: '존재하지 않는 이메일입니다.',
+         });
+      }
+
+      // 요청된 이메일을 찾았다면 비밀번호가 일치하는지 확인
+      user.comparePassword(req.body.password, async (err, isMatch) => {
+         if (err)
+            return res
+               .status(400)
+               .json({
+                  loginSuccess: false,
+                  error: '비밀번호 확인 중 오류가 발생했습니다.',
+               });
+         if (!isMatch) {
+            return res.json({
+               loginSuccess: false,
+               message: '비밀번호가 일치하지 않습니다.',
+            });
+         }
+
+         // 비밀번호가 일치 시, 토큰 생성
+         try {
+            const updatedUser = await user.generateToken(); // 프로미스 반환 처리
+            // 쿠키에 토큰 저장
+            res.cookie('x_auth', updatedUser.token).status(200).json({
+               loginSuccess: true,
+               userId: updatedUser._id,
+            });
+         } catch (err) {
+            return res
+               .status(400)
+               .json({
+                  loginSuccess: false,
+                  error: '토큰 생성 중 오류가 발생했습니다.',
+               });
+         }
+      });
+   } catch (err) {
+      return res.status(500).json({ loginSuccess: false, error: err.message });
    }
 });
 
--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
    name: {
@@ -14,11 +15,12 @@ const userSchema = mongoose.Schema({
    },
    password: {
       type: String,
-      maxlength: 50,
+      minlength: 5,
+      maxlength: 200,
    },
    role: {
       type: Number,
-      default: 0, //1:manager
+      default: 0, // 1: manager
    },
    image: String,
    token: {
@@ -29,10 +31,9 @@ const userSchema = mongoose.Schema({
    },
 });
 
+// 회원정보 저장
 userSchema.pre('save', function (next) {
-   //비밀번호 암호화(npmjs:bcrypt)
    let user = this;
-   console.log(user);
    if (user.isModified('password')) {
       bcrypt.genSalt(saltRounds, (err, salt) => {
          if (err) return next(err);
@@ -47,6 +48,28 @@ userSchema.pre('save', function (next) {
       next();
    }
 });
+
+// 암호화된 DB의 PASSWORD와 사용자가 입력한 PASSWORD를 암호화해서 비교
+userSchema.methods.comparePassword = function (plainPassword, callback) {
+   bcrypt.compare(plainPassword, this.password, (err, isMatch) => {
+      if (err) return callback(err); // 잘못된 부분 수정
+      callback(null, isMatch);
+   });
+};
+
+// 로그인 성공한 user의 토큰 생성
+userSchema.methods.generateToken = async function () {
+   let user = this;
+   let token = jwt.sign(user._id.toHexString(), 'secretToken');
+   user.token = token;
+
+   try {
+      await user.save(); // save() 메서드를 await으로 비동기 처리
+      return user; // 저장된 user를 반환
+   } catch (err) {
+      throw err; // 에러 발생 시 에러를 던짐
+   }
+};
 
 const User = mongoose.model('User', userSchema);
 
