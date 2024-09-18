c6e548141967426b20d9875d145bb003efa180e5
vedivero <vedivero@github.com>
2024-09-18 21:56:29 +0900

    Routes Settings

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'; // Link 유지
+
+import LandingPage from './components/views/LandingPage/LandingPage';
+import LoginPage from './components/views/LoginPage/LoginPage';
+import RegisterPage from './components/views/RegisterPage/RegisterPage'; // 확장자 확인
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <Router>
+         <div>
+            <Routes>
+               <Route path='/' element={<LandingPage />} />
+               <Route path='/login' element={<LoginPage />} />
+               <Route path='/dashboard' element={<RegisterPage />} />
+            </Routes>
+         </div>
+      </Router>
+   );
 }
 
 export default App;
